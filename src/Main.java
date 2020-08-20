d1cfe92901e50ddf9e4241b6469ca9ac26b36a0a
Bettiol <36667512+Bettiol@users.noreply.github.com>
2020-08-20 15:36:28 +0200

    Fixed ArrayList<Assets> add buggy and Created methods DateConverter & dateCompare

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,10 @@ import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.ZoneOffset;
 import java.util.ArrayList;
 
 import org.json.JSONArray;
@@ -29,24 +33,24 @@ public class Main {
 
 			int status = connection.getResponseCode();
 
-			if (status > 299) {
-
+			if (status == 404) {
+				/*
 				reader = new BufferedReader(new InputStreamReader(connection.getErrorStream()));
 				while ((line = reader.readLine()) != null) {
 					responseContent.append(line);
 				}
 				reader.close();
-
+				 */
+				System.out.println("Errore 404");
 			} else {
 				reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
 				while ((line = reader.readLine()) != null) {
 					responseContent.append(line);
 				}
 				reader.close();
-			}
-			//System.out.println(responseContent.toString());
 
-			parse(responseContent.toString());
+				parse(responseContent.toString());
+			}
 
 		} catch (MalformedURLException e) {
 			// TODO Auto-generated catch block
@@ -58,7 +62,7 @@ public class Main {
 
 		/*
 		 JAVA 11+
-		 
+
 		 HttpClient client = HttpClient.newHttpClient();
 		 HttpRequest request = HttpRequest.newBuilder()
 		 	.uri(URI.create("https://api.github.com/repos/Bettiol/GitFlowTest/releases"))
@@ -72,63 +76,77 @@ public class Main {
 	public static void parse(String responseBody) {
 
 		JSONArray informazioni = new JSONArray(responseBody);
-		
-		ArrayList<Asset> assets = new ArrayList<Asset>();
-		
+
+		ArrayList<Asset> assets = null;
+
 		ArrayList<Release> release = new ArrayList<Release>();
-		
-		System.out.println(informazioni.length());
-		
+
+
 		for (int i = 0; i < informazioni.length(); i++) {
-			
+
+			assets = new ArrayList<Asset>();
+
 			JSONObject info = informazioni.getJSONObject(i);
-			
-			
-			String publishat = info.getString("published_at");
+
 			long id = info.getLong("id");
 			String tagname = info.getString("tag_name");
 			String name = info.getString("name");
 			String body = info.getString("body");
-			
-			
-			//System.out.println(publicata);
-			
-			
-			
+
+			String publishatstring = info.getString("published_at");
+			LocalDateTime publishat = dateConverter(publishatstring);
+
 			JSONArray asset = info.getJSONArray("assets");
-			
+
 			for (Object o : asset) {
-			 
+
 				JSONObject jsonLineItem = (JSONObject) o; 
-				
+
 				long idasset = jsonLineItem.getLong("id"); 
 				String nameasset = jsonLineItem.getString("name");
 				long size = jsonLineItem.getLong("size"); 
-				String createdat = jsonLineItem.getString("created_at");
 				String url = jsonLineItem.getString("url");
-				
-				boolean retu = assets.add(new Asset(idasset, nameasset, size, createdat, url));
-				//System.out.println(assets);
+
+				String createdatstring = jsonLineItem.getString("created_at");
+				LocalDateTime createdat = dateConverter(createdatstring);
+
+				boolean statusaddassets = assets.add(new Asset(idasset, nameasset, size, createdat, url));
+				System.out.println("Status ADD assets --> " + statusaddassets);
 			}
-			
-			
-			boolean re = release.add(new Release(id, tagname, name, publishat, assets, body));
-			//System.out.println(assets);
-			
-			assets.clear();
-			
-			//System.out.println(assets);
-			
-					 
+
+			boolean statusaddrelease = release.add(new Release(id, tagname, name, publishat, assets, body));
+			System.out.println("Status ADD release --> " + statusaddrelease);	 
 		}
-		
-		System.out.println(release);
-		/*for(int j = 0; j < release.size(); j++) {
-            System.out.println(release.get(j).toString());
-            System.out.println("\n");
-        }*/
-		
-		
+
+		for(int j = 0; j < release.size(); j++) {
+
+			System.out.println(release.get(j).toString());
+			System.out.println("\n");
+
+		}
+
+
+	}
+
+	public static LocalDateTime dateConverter(String datastring) {
+
+		Instant data = Instant.parse(datastring);
+
+		LocalDateTime dataconvert = LocalDateTime.ofInstant(data, ZoneId.of(ZoneOffset.UTC.getId()));
+
+		return dataconvert;
+	}
+
+	public static void dateCompare(LocalDateTime data1, LocalDateTime data2) {
+
+		boolean isBefore = data1.isBefore(data2);
+		boolean isAfter = data1.isAfter(data2);
+		boolean isEqual = data1.isEqual(data2);
+
+		System.out.println(isBefore);
+		System.out.println(isAfter);
+		System.out.println(isEqual);
+
 	}
 
 }
